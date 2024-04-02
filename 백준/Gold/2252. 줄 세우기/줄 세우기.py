012153c71a839da443abfc9d3c867119d5f9076b
mando <88534959+kssumin@users.noreply.github.com>
2024-04-02 20:58:13 +0900

    [Gold III] Title: 줄 세우기, Time: 4060 ms, Memory: 41068 KB -BaekjoonHub

--- 백준/Gold/2252. 줄 세우기/줄 세우기.py
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.py
@@ -1,27 +1,21 @@
-def bfs():
-    queue = []
+import sys
 
-    # 진입차수가 0인 간선을 모두 넣는다
-    for node in range(1, n + 1):
-        if in_edge[node] == 0:
-            queue.append(node)
-            answer.append(node)
-    while queue:
-        vertex = queue.pop(0)
+sys.setrecursionlimit(999999)
 
-        for next_vertex in arr[vertex]:
-            in_edge[next_vertex] -= 1
+def dfs(node):
+    visited[node] = True
+    for next_vertex in arr[node]:
+        in_edge[next_vertex] -= 1
 
-            if in_edge[next_vertex] == 0:
-                queue.append(next_vertex)
-                answer.append(next_vertex)
-
-    return answer
+        if in_edge[next_vertex] == 0:
+            answer.append(next_vertex)
+            dfs(next_vertex)
 
 
 n, m = map(int, input().split(" "))
 arr = [[] for _ in range(n + 1)]
 in_edge = [0 for _ in range(n + 1)]
+visited = [False for _ in range(n + 1)]
 answer = []
 
 for i in range(m):
@@ -29,4 +23,10 @@ for i in range(m):
     arr[x].append(y)
     in_edge[y] += 1
 
-print(*(bfs()))
+# 진입차수가 0인 간선을 모두 넣는다
+for node in range(1, n + 1):
+    if in_edge[node] == 0 and visited[node]==False:
+        answer.append(node)
+        dfs(node)
+
+print(*answer)
