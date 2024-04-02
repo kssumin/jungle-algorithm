813f9b3fa7188c007eecf837d6fc7e936e2bfb0d
mando <88534959+kssumin@users.noreply.github.com>
2024-04-02 20:29:16 +0900

    [Gold III] Title: 줄 세우기, Time: 304 ms, Memory: 117748 KB -BaekjoonHub

---
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.py
@@ -0,0 +1,32 @@
+def bfs():
+    queue = []
+
+    # 진입차수가 0인 간선을 모두 넣는다
+    for node in range(1, n + 1):
+        if in_edge[node] == 0:
+            queue.append(node)
+            answer.append(node)
+    while queue:
+        vertex = queue.pop(0)
+
+        for next_vertex in arr[vertex]:
+            in_edge[next_vertex] -= 1
+
+            if in_edge[next_vertex] == 0:
+                queue.append(next_vertex)
+                answer.append(next_vertex)
+
+    return answer
+
+
+n, m = map(int, input().split(" "))
+arr = [[] for _ in range(n + 1)]
+in_edge = [0 for _ in range(n + 1)]
+answer = []
+
+for i in range(m):
+    x, y = map(int, input().split(" "))
+    arr[x].append(y)
+    in_edge[y] += 1
+
+print(*(bfs()))
