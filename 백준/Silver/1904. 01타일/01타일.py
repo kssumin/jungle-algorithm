ca4981d19099db3ffce1a44740e025dbfb66267f
mando <88534959+kssumin@users.noreply.github.com>
2024-04-04 22:48:40 +0900

    [Silver III] Title: 01타일, Time: 124 ms, Memory: 117056 KB -BaekjoonHub

---
+++ 백준/Silver/1904. 01타일/01타일.py
@@ -0,0 +1,13 @@
+n = int(input())
+d = [0] * (n + 2)
+
+d[1] = 1
+d[2] = 2
+
+
+def fibo(n):
+    for i in range(3, n + 1):
+        d[i] = (d[i - 1] + d[i - 2]) % 15746
+    return d[n]
+
+print(fibo(n))
