c61222ea38b4091c06a4ac0d747e0ae750e48db8
mando <88534959+kssumin@users.noreply.github.com>
2024-05-16 18:39:02 +0900

    [level 2] Title: 괄호 회전하기, Time: 0.01 ms, Memory: 9.96 MB -BaekjoonHub

--- 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.py
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.py
@@ -6,47 +6,52 @@ def solution(s):
     q = deque(s)
     size = len(s)
     
-    while(size !=0):
-        # 이 부분이 하나씩 완쪽으로 회전시키는 부분
-        item = q.popleft()
-        q.append(item)
+    # while(size !=0):
+    #     item = q.popleft()
+    #     q.append(item)
         
-        # 해당 문자열이 올바른지 확인하는 부분
-        if(check(q, len(q))):
+    if(check(q, size)):
             answer+=1
         
-        size-=1
+        # size-=1
         
     return answer
 
 def check(q, size):
     stack = []
     
-    for i in range(len(q)):
-        item = q[i]
-        # 여는 괄호라면
-        if item =='[' or item=='(' or item=='{':
-            stack.append(item)
-        # 닫는 괄효라면
-        else:
-            # 스택이 비어있다면 즉, 여는 괄호가 없을 경우는 짝이 맞지 않는다
-            if not stack:
-                return False
-            
-            item_pop = stack.pop()
-            
-            if item_pop=='[' and item ==']':
-                continue
-            if item_pop=='(' and item ==')':
-                continue
-            if item_pop=='{' and item =='}':
-                continue
-            
-            return False
+    if size == 0:
+        return False
+
+    print(q)
+    print(q.popleft())
+    print(q.popleft())
     
-    # 스택이 비어있다면 짝이 맞는거다
-    if not stack:
-        return True
-    return False
-        
+    # for i in range(size):
+    #     item = q.popleft()
+    #     print(item)
+        # 여는 괄호라면
+#         if item =='[' or item=='(' or item=='{':
+#             stack.append(item)
+#         # 닫는 괄효라면
+#         else:
+#             # 스택이 비어있다면
+#             if not stack:
+#                 # 여는 괄호가 없다는 뜻이므로 실패
+#                 return False
             
+#             # item_pop = stack.pop()
+#             item_pop = 0
+            
+#             if item_pop=='[' and item ==']':
+#                 continue
+#             elif item_pop=='(' and item ==')':
+#                 continue
+#             elif item_pop=='{' and item =='}':
+#                 continue
+#             else:
+#                 return False
+    
+#     if not stack:
+#         return True
+#     return False
