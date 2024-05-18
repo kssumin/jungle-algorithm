2771ff3dc6c9d3229921a24be664aa3845c1e3a6
mando <88534959+kssumin@users.noreply.github.com>
2024-05-18 12:00:20 +0900

    [level 2] Title: 시소 짝꿍, Time: 0.03 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/152996. 시소 짝꿍/README.md
+++ 프로그래머스/2/152996. 시소 짝꿍/README.md
@@ -1,10 +1,10 @@
 # [level 2] 시소 짝꿍 - 152996 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/152996#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/152996) 
 
 ### 성능 요약
 
-메모리: 10.4 MB, 시간: 0.06 ms
+메모리: 10.1 MB, 시간: 0.03 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 05월 06일 13:25:33
+2024년 05월 18일 12:00:19
 
 ### 문제 설명
 
--- 프로그래머스/2/152996. 시소 짝꿍/시소 짝꿍.py
+++ 프로그래머스/2/152996. 시소 짝꿍/시소 짝꿍.py
@@ -4,20 +4,13 @@ def solution(weights):
     answer = 0
     
     counter = Counter(weights)
+    # print(counter)
     
-    max_value = max(weights)
-    min_value = min(weights)
-        
-    for weight in range(min_value, max_value+1):
-        # 해당 몸무게가 존재한다면
-        if counter[weight] >= 1:
-            self_weight = counter[weight] 
-            
-            answer = answer + (counter[weight * (2/3)] * self_weight)
-            answer = answer + (counter[weight * (1/2)] * self_weight)
-            answer = answer + (counter[weight * (3/4)] * self_weight)
-                  
-            answer = answer + ((self_weight * (self_weight -1))//2)
-
-            
+    for weight, num in counter.items():
+        # print(weight, num)
+        answer = answer + counter[weight*(1/2)] * num
+        answer = answer + counter[weight*(2/3)] * num
+        answer = answer + counter[weight*(3/4)] * num
+        answer = answer + ((num * (num - 1)) / 2)
+        print(answer)
     return answer
