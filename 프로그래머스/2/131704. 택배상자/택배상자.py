fbde3f5c99dca69997228d0f6e2e27d6c1bb191c
mando <88534959+kssumin@users.noreply.github.com>
2024-05-22 10:22:15 +0900

    [level 2] Title: 택배상자, Time: 280.56 ms, Memory: 53.1 MB -BaekjoonHub

--- 프로그래머스/2/131704. 택배상자/택배상자.py
+++ 프로그래머스/2/131704. 택배상자/택배상자.py
@@ -1,18 +1,41 @@
 def solution(order):
     answer = 0
-    stack = []
-    size = len(order)
-    # 현 트럭에 담아야 하는 상자의 인덱스
+    
+    container = [i for i in range(1, len(order)+1)]
+    temp_c = []
+    
     index = 0
     
-    for box in range(1, size+1):
-        stack.append(box)
+    print(container)
+    print(container.pop(0))
+    print(container)
+    
+    stack=[1,2,3]
+    print(stack)
+    print(stack.append(4))
+    print(stack)
+#     while(true):
+#         # 컨테이너는 모두 다 봤을 때 이제 임시 컨테이너만 봐야함
+#         if index >= len(order):
+#             return answer
         
-        # 현 임시 컨테이너에 있는 것을 트럭에 담아야 한다
-        while (stack and stack[-1] == order[index]):
-            answer+=1
-            index+=1
-            stack.pop()
+#         # 컨테이너도 볼 상자가 남음
+#         else:
+#             # 컨테이너에 현 주문이 있음
+#             if container[0] == order[0]:
+#                 answer+=1
             
+#             # 임시컨테이너에 현 주문이 있음
+#             elif temp_c[0] == order[0]:
+#                 temp_c.pop(0)
+#                 answer+1
+            
+#             # 임시 컨테이너와 컨테이너 모두 현 주문이 없음
+#             else:
+#                 temp_c.append(container.pop(0))
+                
+        
+        
+        
+    
     return answer
-            
