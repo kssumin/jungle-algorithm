04e2c6650a5e784e8d36f70f6d4ef28f22d094b3
mando <88534959+kssumin@users.noreply.github.com>
2024-05-31 11:02:12 +0900

    [level 2] Title: 리코쳇 로봇, Time: 0.96 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/169199. 리코쳇 로봇/리코쳇 로봇.py
+++ 프로그래머스/2/169199. 리코쳇 로봇/리코쳇 로봇.py
@@ -1,60 +1,39 @@
-dx = [-1, 1, 0, 0]
-dy = [0, 0, -1, 1]
-
-queue = []
+dx = [-1,1,0,0]
+dy=[0,0,-1,1]
 
 def solution(board):
+    answer = 0
+    
     row = len(board[0])
     column = len(board)
     
-    visited = [[False for _ in range(row)] for _ in range(column)]
-    distances = [[-1 for _ in range(row)] for _ in range(column)]
+    visited = [[False for _ in range(column)] for _ in range(row)]
     
-    for i in range(column):
-        for j in range(row):
-            if board[i][j] == 'R':
-                visited[i][j] = True
-                distances[i][j] = 0
-                queue.append((i, j))
-                answer = search(board, visited, distances, column, row)
-                return answer if answer != -1 else -1
+    print(visited)
+    print(row, column)
+    print(board[0][1])
     
-    return -1
-
-def search(board, visited, distances, column, row):
-    while queue:
-        i, j = queue.pop(0)
-        
-        for k in range(4):
-            next_x, next_y = i, j
-            while True:
-                next_x = next_x + dx[k]
-                next_y = next_y + dy[k]
-        
-                if is_not_range(next_x, next_y, column, row):
-                    next_x, next_y = next_x - dx[k], next_y - dy[k]
-                    break
-        
-                if can_not_go(next_x, next_y, board):
-                    next_x, next_y = next_x - dx[k], next_y - dy[k]
-                    break
-            
-            if not visited[next_x][next_y]:
-                visited[next_x][next_y] = True
-                distances[next_x][next_y] = distances[i][j] + 1
-                queue.append((next_x, next_y))
-        
-                if board[next_x][next_y] == 'G':
-                    return distances[next_x][next_y]
     
-    return -1
+    for i in range(row):
+        for j in range(column):
+            if board[i][j] == 'G':
+                dfs(i,j)
+    
+    return answer
 
-def is_not_range(i, j, column, row):
-    if i >= column or j >= row or i < 0 or j < 0:
-        return True
-    return False
 
-def can_not_go(i, j, board):
-    if board[i][j] == 'D':
-        return True
-    return False
+def dfs(i,j):
+    for k in range(4):
+        new_x = i + dx[k]
+        new_y = i + dy[k]
+        
+        if is_not_range(new_x, new_y):
+            continue
+        
+
+
+def is_not_range(i,j):
+    global column, row
+    if i>=column or j>=row:
+        return true
+    return false
