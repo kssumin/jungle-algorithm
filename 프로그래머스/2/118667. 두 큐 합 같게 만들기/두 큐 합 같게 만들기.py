9de0e0db7e690ac0c48de2858f79de5ea429fc62
mando <88534959+kssumin@users.noreply.github.com>
2024-06-02 21:35:09 +0900

    [level 2] Title: 두 큐 합 같게 만들기, Time: 45.34 ms, Memory: 19.3 MB -BaekjoonHub

--- 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.py
+++ 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.py
@@ -1,34 +1,29 @@
 from collections import deque
 
 def solution(queue1, queue2):
-    # 큐를 deque로 변환하여 효율적인 pop, append 연산을 사용
-    queue1 = deque(queue1)
-    queue2 = deque(queue2)
+    max = len(queue1) + len(queue2) 
+    answer = 0
     
-    sum1 = sum(queue1)
-    sum2 = sum(queue2)
-    total_sum = sum1 + sum2
-    
-    # 전체 합이 홀수면 반으로 나눌 수 없으므로 -1 반환
-    if total_sum % 2 != 0:
+    # 우선 반이 안 되는지 확인
+    if (sum(queue1) + sum(queue2)) % 2 != 0:
         return -1
     
-    target = total_sum // 2
-    max_operations = 3 * len(queue1)
-    operations = 0
+    while(sum(queue1) != sum(queue2)):
+        if answer > max:
+            return -1
     
-    while sum1 != target and operations <= max_operations:
-        if sum1 > target:
-            # queue1의 앞에서 요소를 제거하여 queue2에 추가
-            item = queue1.popleft()
-            sum1 -= item
+    
+        if sum(queue1) > sum(queue2):
+            item = queue1.pop(0)
             queue2.append(item)
+            answer+=1
+            
         else:
-            # queue2의 앞에서 요소를 제거하여 queue1에 추가
-            item = queue2.popleft()
-            sum2 -= item
+            item = queue2.pop(0)
             queue1.append(item)
-            sum1 += item
-        operations += 1
+            answer+=1
     
-    return operations if sum1 == target else -1
+    return answer
+            
+            
+        
